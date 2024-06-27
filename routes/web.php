8fc5d8998f3d580bdf2ce2adb327f11da9865f15
DC <dcsky185@163.com>
2024-06-27 19:02:57 +0800

    环境搭建

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Counter;
+use App\Livewire\CreatePost;
+use App\Livewire\Posts;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/lw/counter", Counter::class);
+Route::get("/lw/post", CreatePost::class);
+Route::get("/lw/posts", Posts::class);
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
